62848585e228ff63069b3d975cb21ec819f1a337
Ana Clara <anaclara.cavalcante.2007@gmail.com>
2026-04-10 16:16:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  const nome = "Ana Clara";
+
+  return (
+    <div>
+      <h1>Hello, {nome}!</h1>
+      <p>teste</p>
+    </div>
+  );
+}
+
+export default App;
+ 
