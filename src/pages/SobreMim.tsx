3ddb8f77a032ce7dc6ce5e546af2c9316d7941ea
Ana Clara <anaclara.cavalcante.2007@gmail.com>
2026-04-12 12:41:43 -0300

    feat: cria a página Sobre Mim

--- src/pages/SobreMim.tsx
+++ src/pages/SobreMim.tsx
@@ -1,5 +1,50 @@
+import styles from "./SobreMim.module.css";
+
 function SobreMim() {
-  return <h2>Sobre mim</h2>;
+  return (
+    <section className={styles.sobreMimContainer}>
+      <h1>Sobre mim</h1>
+
+      <section className={styles.infoContainer}>
+        <article className={styles.textBlock}>
+          <h2>lorem</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+            ipsam ratione dolores debitis. Cupiditate doloribus animi, provident
+            sunt corrupti fuga, exercitationem consequatur vero consequuntur
+            asperiores, error necessitatibus incidunt! Aut!
+          </p>
+        </article>
+        <article className={styles.textBlock}>
+          <h2>lorem</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+            ipsam ratione dolores debitis. Cupiditate doloribus animi, provident
+            sunt corrupti fuga, exercitationem consequatur vero consequuntur
+            asperiores, error necessitatibus incidunt! Aut!
+          </p>
+        </article>
+        <article className={styles.textBlock}>
+          <h2>lorem</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+            ipsam ratione dolores debitis. Cupiditate doloribus animi, provident
+            sunt corrupti fuga, exercitationem consequatur vero consequuntur
+            asperiores, error necessitatibus incidunt! Aut!
+          </p>
+        </article>
+        <article className={styles.textBlock}>
+          <h2>lorem</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+            ipsam ratione dolores debitis. Cupiditate doloribus animi, provident
+            sunt corrupti fuga, exercitationem consequatur vero consequuntur
+            asperiores, error necessitatibus incidunt! Aut!
+          </p>
+        </article>
+      </section>
+    </section>
+  );
 }
 
 export default SobreMim;
