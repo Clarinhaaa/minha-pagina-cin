d2cdfd750184efc413a5cddcdf58b6750a08abc3
Ana Clara <anaclara.cavalcante.2007@gmail.com>
2026-04-12 12:42:17 -0300

    refactor: faz mais padronizações de CSS e HTML semântico

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,15 @@ import foto from "../assets/foto.png"
 
 function Home() {
   return (
-    <div className={styles.homeContainer}>
+    <section className={styles.homeContainer}>
       <h1>Ana Clara de Oliveira Cavalcante</h1>
       <h2>Sistemas de Informação (2° período)</h2>
 
-      <div className={styles.infoBasica}>
+      <article className={styles.infoBasica}>
         <img src={foto} alt="Foto minha" />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ullam deserunt temporibus aspernatur! Tempora voluptatem numquam quos, molestias ex veniam laudantium nostrum perspiciatis ipsam repellendus a voluptatum labore? Recusandae, nobis.</p>
-      </div>
-
-    </div>
+      </article>
+    </section>
   );
 }
 
