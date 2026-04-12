9859498780dee5961dca90ca2fbc6a6143aef038
Ana Clara <anaclara.cavalcante.2007@gmail.com>
2026-04-11 22:58:37 -0300

    feat: cria o footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Footer.module.css";
+
 function Footer() {
+  function showWarning(param: string): void {
+    window.alert(`Contato: ${param}`);
+  }
+
   return (
-    <footer>
+    <footer className={styles.footerContainer}>
+      <div className={styles.contact}>
+        <a href="https://github.com/Clarinhaaa" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="xl" />
+        </a>
+        <a href="https://www.linkedin.com/in/ana-clara-cavalcante-83ba59374/" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} size="xl" />
+        </a>
+        <a href="#" onClick={() => showWarning("acoc@cin.ufpe.br")}>
+          <FontAwesomeIcon icon={faEnvelope} size="xl" />
+        </a>
+        <a href="#" onClick={() => showWarning("(82) 9 9910-6186")}>
+          <FontAwesomeIcon icon={faPhone} size="xl" />
+        </a>
+      </div>
       <p>
-        Favicon found in
-        <a href="https://www.svgrepo.com/svg/407540/sun">SVG Repo</a>
+        Favicon found in{" "}
+        <a href="https://www.svgrepo.com/svg/407540/sun" target="_blank" style={{ padding: 0 }}>
+          SVG Repo
+        </a>
       </p>
     </footer>
   );
